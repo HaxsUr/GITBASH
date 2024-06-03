ad8e5f871f62c268a5067a6566ace4efe6dd87a5
HaxsUr <m4d4nhaja@gmail.com>
2024-06-03 10:27:28 +0800

    menambah main class

--- src/PBO/Index.java
+++ src/PBO/Index.java
@@ -1,13 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package PBO;
-
-/**
- *
- * @author harim
- */
 public class Index {
-    
+    public static void main(String[] args) {
+        System.out.print("hello");
+    }
 }
